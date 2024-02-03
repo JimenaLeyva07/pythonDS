b038c4782d645562c5e63b635cd157cd3a380074
Jimena Leyva <115754127+JimenaLeyva07@users.noreply.github.com>
2024-02-02 20:49:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,12 @@ import pandas as pd
 
 import numpy as np 
 
-st.title('Titulo') 
+st.title('UPC Data Science 2024')
+
+st.header('Simulador Ventas')
+
+n = st.slider("cant. ventas", 5,100, step=1)
+
+chart_data = pd.DataFrame(np.random.randn(n),columns=['ventas'])
+
+st.line_chart(chart_data)
